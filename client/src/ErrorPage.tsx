ffd63c76d2fea71ac2b14279f36b673b019b17a1
Matthew Gaddes <matt.gaddes@datagrail.io>
2024-09-27 15:55:33 -0700

    Add react-router with initial route & basic error page

---
+++ client/src/ErrorPage.tsx
@@ -0,0 +1,17 @@
+import { useRouteError } from "react-router-dom";
+
+export const ErrorPage = () => {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        {/* @ts-expect-error - `unknown` error type imported from react-router-dom */}
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
+  );
+};
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import { App } from "./App.tsx";
+import { ErrorPage } from "./ErrorPage.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
