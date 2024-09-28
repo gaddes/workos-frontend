0dc2e69d7675068f4719004027921a0a0128cce0
Matthew Gaddes <matt.gaddes@datagrail.io>
2024-09-27 18:39:36 -0700

    Use custom Radix theme

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,17 @@
+import "@radix-ui/themes/styles.css";
+import "./main.css";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { Theme } from "@radix-ui/themes";
 
 import { router } from "./main.routes.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Theme accentColor="violet">
+      <RouterProvider router={router} />
+    </Theme>
   </StrictMode>,
 );
--- client/src/pages/Admin/Users.tsx
+++ client/src/pages/Admin/Users.tsx
@@ -1,1 +1,8 @@
-export const Users = () => <div>Users</div>;
+import { Flex, Text, Button } from "@radix-ui/themes";
+
+export const Users = () => (
+  <Flex direction="column" gap="2">
+    <Text>Hello from Radix Themes :)</Text>
+    <Button>Let's go</Button>
+  </Flex>
+);
